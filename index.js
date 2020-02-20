cbccfebbe0d07444cbc878daabfc576b0bb5a202
Lolo971971 <61194543+Lolo971971@users.noreply.github.com>
2020-02-20 23:59:39 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const Discord = require("discord.js");
-const Fs = require("fs")
+const fs = require("fs")
 const client = new Discord.Client();
 
 client.login(process.env.TOKEN);
 
-console.log("Le bot à été lancé")
+console.log("Le bot à été lancé");
 
 
-client.commands = new DiscordCollection();
+client.commands = new Discord.Collection();
 
-fs.readir((error, f) =>{
+fs.readdir((error, f) =>{
     if(error) console.error("Les commandes n'ont pas pu être chargé");
     
-    let commands = f.filter(f => f.split(".").pop() === "js");
+    let commandes = f.filter(f => f.split(".").pop() === "js");
+
+    commandes.forEach((f) =>{
+
+        let commande = require('${f}');
+        
+        console.log('${f.lenght} en chargement');
+        client.set(commands.help.name, commande)
+    });
+});
+
+fs.readdir((error, f) =>{
+
 })
