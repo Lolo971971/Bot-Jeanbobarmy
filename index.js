dd2ca7b064ebd712aa5959b44f241499d971925b
Lolo971971 <61194543+Lolo971971@users.noreply.github.com>
2020-02-20 16:36:35 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login("Njc5MzA0MTYyMTE0MjczMjkw.Xkwa4g.KnCfhcdF5ecD2E0jvneGE317ZWc");
+client.login(process.env.TOKEN);
